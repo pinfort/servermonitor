a8b6daca196d7966e84c609d739f8d87af9cf60c
pinfort <ptg@nijitei.com>
2021-01-24 01:05:16 +0900

    update

--- src/main/kotlin/me/pinfort/servermonitor/component/statusReport/StatusPage.kt
+++ src/main/kotlin/me/pinfort/servermonitor/component/statusReport/StatusPage.kt
@@ -1,9 +1,58 @@
 package me.pinfort.servermonitor.component.statusReport
 
+import kotlinx.coroutines.runBlocking
+import me.pinfort.servermonitor.config.ServerCheckConfigurationProperties
+import me.pinfort.servermonitor.entity.IncidentApi
+import me.pinfort.servermonitor.enum.ServerStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+import java.net.URI
 
 @Component
-class StatusPage() {
-    fun report(targets: List<String>) {
+class StatusPage(
+    private val serverCheckConfigurationProperties: ServerCheckConfigurationProperties,
+    private val webClient: WebClient
+) {
+    fun report(targets: List<Pair<String, ServerStatus>>) {
+        val statuses: MutableList<Pair<String, ServerStatus>> = mutableListOf()
+        targets.filter { it.second != ServerStatus.LIVE }.forEach {
+            val component = serverCheckConfigurationProperties.components.find { component -> it.first == component.name }
+                ?: return@forEach
+            statuses.add(component.componentId to it.second)
+        }
+        if (statuses.isEmpty()) {
+            return
+        }
+        val incidentName: String = statuses.joinToString(", ") {
+            serverCheckConfigurationProperties.components.find { component -> it.first == component.componentId }?.name
+                ?: ""
+        } + " outage"
+        runBlocking {
+            val job = kickApi(statuses.map { it.first }, incidentName)
+            val responseEntity = job.block()
+            if (responseEntity?.statusCode?.is2xxSuccessful != true) {
+                // error
+            }
+        }
+    }
+
+    suspend fun kickApi(componentIds: List<String>, incidentName: String): Mono<ResponseEntity<Void>> {
+        val components: MutableMap<String, String> = mutableMapOf()
+        componentIds.forEach { components.put(it, "major_outage") }
+        val incidentObject = IncidentApi(
+            name = incidentName,
+            status = "investigating",
+            impactOverride = "critical",
+            body = "This is incident created by monitoring system. Admin will check and update this.",
+            components = components.toMap(),
+            componentIds = componentIds
+        )
+        val pageId = serverCheckConfigurationProperties.pageId
+        return webClient.post().uri(URI("https://api.statuspage.io/v1/pages/${pageId}/incidents"))
+            .body(BodyInserters.fromValue(incidentObject))
+            .retrieve().toBodilessEntity()
     }
 }
--- src/main/kotlin/me/pinfort/servermonitor/service/ServerCheckSchedule.kt
+++ src/main/kotlin/me/pinfort/servermonitor/service/ServerCheckSchedule.kt
@@ -1,17 +1,22 @@
 package me.pinfort.servermonitor.service
 
 import me.pinfort.servermonitor.component.statusCheck.http.Head
+import me.pinfort.servermonitor.component.statusReport.StatusPage
 import me.pinfort.servermonitor.config.ServerCheckConfigurationProperties
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
 @Service
 @EnableScheduling
 class ServerCheckSchedule(
-    private val serverCheckConfigurationProperties: ServerCheckConfigurationProperties,
     private val httpHead: Head,
+    private val statusPage: StatusPage
 ) {
+    @Scheduled(cron = "12 13 * * * *")
     fun execute() {
         val results = httpHead.check()
+        val statuses = results.map { it.name to it.status }
+        statusPage.report(statuses)
     }
 }
